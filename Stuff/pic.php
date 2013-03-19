5c061bb0b62a9b4a68a822cd8e21d5ba9a1bd285
nohe427 <an96387@gulls.salisbury.edu>
2013-03-18 21:54:47 -0400

    Added the connection Include file

--- Stuff/pic.php
+++ Stuff/pic.php
@@ -2,7 +2,10 @@
 <?php
 session_start();
 
-	$connect = mysql_connect("localhost","apnohe","o79gmtype") or die("Could not connect");
+	//$connect = mysql_connect("localhost","apnohe","o79gmtype") or die("Could not connect");
+
+Include "connection.php";
+
 	mysql_select_db("PROJECT2_NOHE") or die("Could not find DB");
 	$id = $_REQUEST['picid'];
 	//$image_query = "SELECT * FROM CLAIM WHERE CLAIM_NUM = '$id'";
