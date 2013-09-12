6d28a57a67ada7a0a51a140206c08d92349d0104
alex.nohe427@gmail.com <alex7370@ALEXN.esri.com>
2013-09-12 12:28:26 -0700

    modified:   getImage.php

--- Stuff/getImage.php
+++ Stuff/getImage.php
@@ -1,14 +1,11 @@
 <?php
 
 // Address error handling
-
 ini_set('display_errors',1);
 error_reporting(E_ALL & ~E_NOTICE);
 //Attempt to Connect
-
 //if ($connection = @mysql_connect ('localhost', 'apnohe', 'o79gmtype')){
-	//print '<p>Successfully connected to MySQL.</p>';
-	 
+//print '<p>Successfully connected to MySQL.</p>';
 //}
 //else {
 //	die('<p>Could not connect to MySQL because: <b>' //.mysql_error() .
@@ -21,8 +18,6 @@ if (@mysql_select_db("PROJECT2_NOHE", $connection)){
 else {
 	die('<p>Cound not select the flagg alexamara database because: <b>' .mysql_error().'</b></p>');
 }
-
-
 $id = $_REQUEST['id'];
 //Possibly change to "SELECT * FROM CLAIM WHERE CLAIM_NUM = ".$id." to carry over the variable?
 $image = mysql_query("SELECT * FROM CLAIM WHERE CLAIM_NUM = $id");
@@ -33,5 +28,4 @@ $filename=$imageArray['filename'];
 $ext=end(explode('.',$filename));
 header("context-type:$ext");
 echo $image;
-
 ?>
